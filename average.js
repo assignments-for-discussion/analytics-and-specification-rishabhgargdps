2aba7909921fbe1417bf60016940dd57f7b5036a
rishabhgarg <rishabhgargdps@gmail.com>
2021-12-12 08:50:27 +0530

    feature: high variation report

--- average.js
+++ average.js
@@ -1,6 +1,22 @@
 function average(numbers) {
-    const array_filter_NaN = numbers.filter(value => !Number.isNaN(value));
-    return array_filter_NaN.reduce((p, c) => p + c, 0) / array_filter_NaN.length;
+    //average is computed as sum / count where count excludes the NaN
+    let sum = 0;
+    let count = numbers.length;
+    let max_value = Math.max(...numbers);
+    let min_value = Math.min(...numbers);
+    let variation = max_value - min_value; // variation is computed as maximum number - minimum number
+    if (variation > 0.95 * max_value) {
+        return NaN;
+    }
+
+    numbers.map((val, i) => {
+        if (isNaN(val)) {
+            count -= 1
+        } else {
+            sum += val;
+        }
+    })
+    return sum / count
 }
 
 module.exports = { average };
