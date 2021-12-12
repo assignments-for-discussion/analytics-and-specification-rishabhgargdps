596fd870d82325738b17f2c276cc8e9f527432eb
rishabhgarg <rishabhgargdps@gmail.com>
2021-12-12 08:28:59 +0530

    fix: average.js by excluding NaN

--- average.js
+++ average.js
@@ -1,6 +1,17 @@
 function average(numbers) {
-    return numbers.reduce((p, c) => p + c, 0) / numbers.length;
-    const newArr = numbers.filter(value => !Number.isNaN(value));
+    //average is computed as sum / count where count excludes the NaN values
+    let res = 0;
+    let count = numbers.length;
+
+    numbers.map((val, i) => {
+        if (isNaN(val)) {
+            count -= 1
+        } else {
+            res += val;
+        }
+    })
+
+    return res / count
 }
 
 module.exports = { average };
