4ceba8ddcfce63e5b95844a1a888fdbbc52e85bc
rishabhgarg <rishabhgargdps@gmail.com>
2021-12-12 08:58:50 +0530

    concise code

--- average.js
+++ average.js
@@ -1,23 +1,12 @@
 function average(numbers) {
-    //average is computed as sum / count where count excludes the NaN
-    let sum = 0;
-    let count = numbers.length;
+    //filtering for NaN
+    const filterNaN = numbers.filter(value => !Number.isNaN(value));
     let maxValue = Math.max(...numbers);
     let minValue = Math.min(...numbers);
     let variation = maxValue - minValue;
     // variation is computed as maximum number - minimum number
-    if (variation > 0.95 * maxValue) {
-        return NaN;
-    }
-
-    numbers.map((val, i) => {
-        if (isNaN(val)) {
-            count -= 1
-        } else {
-            sum += val;
-        }
-    })
-    return sum / count
+    if (variation > 0.95 * maxValue) return NaN;
+    return filterNaN.reduce((p, c) => p + c, 0) / filterNaN.length;
 }
 
 module.exports = { average };
